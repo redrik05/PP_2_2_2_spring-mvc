f7ffc26d5825761071cff1ac74bf4cffd0627cad
Redrik <79138016+redrik05@users.noreply.github.com>
2024-08-05 19:58:20 +0300

    Update CarServiceImpl.java

--- src/main/java/web/service/CarServiceImpl.java
+++ src/main/java/web/service/CarServiceImpl.java
@@ -9,12 +9,12 @@ import java.util.List;
 @Component
 public class CarServiceImpl implements CarService {
 
-    Car car1 = new Car("Lada", 2110, "white");
-    Car car2 = new Car("GAZ", 3110, "black");
-    Car car3 = new Car("Ford", 200, "white");
-    Car car4 = new Car("Land Cruiser", 200, "yellow");
-    Car car5 = new Car("Ferrari", 430, "red");
-    List<Car> cars = new ArrayList<>();
+    private Car car1 = new Car("Lada", 2110, "white");
+    private Car car2 = new Car("GAZ", 3110, "black");
+    private Car car3 = new Car("Ford", 200, "white");
+    private Car car4 = new Car("Land Cruiser", 200, "yellow");
+    private Car car5 = new Car("Ferrari", 430, "red");
+    private List<Car> cars = new ArrayList<>();
 
     @Override
     public List<Car> showCars(int count) {
